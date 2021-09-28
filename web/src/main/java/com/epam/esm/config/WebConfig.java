8a4b3a20204d69692856c550c8c3e106c2e357f8
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-09-28 21:26:05 +0300

    Add web app configurations

---
+++ web/src/main/java/com/epam/esm/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.epam.esm.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.epam.esm")
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.enableContentNegotiation(new MappingJackson2JsonView());
+    }
+}
