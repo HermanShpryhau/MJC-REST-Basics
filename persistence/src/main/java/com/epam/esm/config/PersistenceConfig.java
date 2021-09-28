099ef27d04deadb1b28991efb938ca2f756772e1
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-09-28 23:30:08 +0300

    Add persistence config

---
+++ persistence/src/main/java/com/epam/esm/config/PersistenceConfig.java
@@ -0,0 +1,48 @@
+package com.epam.esm.config;
+
+import com.epam.esm.domain.GiftCertificate;
+import com.epam.esm.domain.Tag;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:db.properties")
+public class PersistenceConfig {
+    @Bean
+    public DataSource dataSource(@Value("${jdbc.username}") String username,
+                                 @Value("${jdbc.password}") String password,
+                                 @Value("${jdbc.driverClassName}") String className,
+                                 @Value("${jdbc.url}") String connectionUrl,
+                                 @Value("${jdbc.connections}") Integer connectionsNumber) {
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        basicDataSource.setDriverClassName(className);
+        basicDataSource.setUrl(connectionUrl);
+        basicDataSource.setMaxActive(connectionsNumber);
+        return basicDataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public RowMapper<GiftCertificate> giftCertificateRowMapper() {
+        return new BeanPropertyRowMapper<>(GiftCertificate.class);
+    }
+
+    @Bean
+    public RowMapper<Tag> tagRowMapper() {
+        return new BeanPropertyRowMapper<>(Tag.class);
+    }
+}
