42ef646002278749651a2fa98d151f3549e1c6e1
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-09-29 11:06:39 +0300

    Add gift certificate-specific data access methods

--- persistence/src/main/java/com/epam/esm/persistence/repository/GiftCertificateRepository.java
+++ persistence/src/main/java/com/epam/esm/persistence/repository/GiftCertificateRepository.java
@@ -1,6 +1,18 @@
 package com.epam.esm.persistence.repository;
 
 import com.epam.esm.domain.GiftCertificate;
+import com.epam.esm.domain.Tag;
+
+import java.util.List;
 
 public interface GiftCertificateRepository extends CrudRepository<GiftCertificate> {
+    List<GiftCertificate> findByPatternInNameOrDescription(String pattern);
+
+    List<GiftCertificate> findByTagName(String tagName);
+
+    List<Tag> findAssociatedTags(Long certificateId);
+
+    void addTagAssociation(Long certificateId, Long tagId);
+
+    void removeTagAssociation(Long certificateId, Long tagId);
 }
