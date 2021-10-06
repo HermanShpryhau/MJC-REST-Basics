d3b33d0d5628703ffa005801912864d859af397e
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-07 00:41:18 +0300

    Fix DTO tag validation

--- model/src/main/java/com/epam/esm/domain/validation/DtoTagValidator.java
+++ model/src/main/java/com/epam/esm/domain/validation/DtoTagValidator.java
@@ -1,6 +1,7 @@
 package com.epam.esm.domain.validation;
 
 import com.epam.esm.domain.Tag;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
@@ -8,10 +9,7 @@ public class DtoTagValidator implements ConstraintValidator<DtoTag, Tag> {
     @Override
     public boolean isValid(Tag tag, ConstraintValidatorContext context) {
         boolean valid = true;
-        if (!((tag.getName() != null
-                && tag.getName().length() < 45
-                && tag.getName().length() > 1)
-                || (tag.getId() != null))) {
+        if (tag.getName() == null || tag.getName().length() < 45 || tag.getName().length() > 1) {
             context.buildConstraintViolationWithTemplate("{tag.dto.constraint.message}");
             valid = false;
         }
