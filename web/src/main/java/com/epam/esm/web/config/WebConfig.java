e9b68685c256ec273976aacbc1bace9cec849246
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-06 22:53:29 +0300

    Configure error messages source

--- web/src/main/java/com/epam/esm/web/config/WebConfig.java
+++ web/src/main/java/com/epam/esm/web/config/WebConfig.java
@@ -1,7 +1,14 @@
 package com.epam.esm.web.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,9 +17,26 @@ import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.epam.esm")
+@PropertySource("classpath:/webapp.properties")
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.enableContentNegotiation(new MappingJackson2JsonView());
+    }
+
+    @Bean
+    @Qualifier("errorMessageSource")
+    public MessageSource messageSource(
+            @Value("${app.errorMessagesFilename}") String resourceBundleBaseName,
+            @Value("${app.defaultEncoding}") String defaultEncoding) {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename(resourceBundleBaseName);
+        source.setDefaultEncoding(defaultEncoding);
+        return source;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 }
