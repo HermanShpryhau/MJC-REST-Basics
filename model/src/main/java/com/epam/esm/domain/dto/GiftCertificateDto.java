d0f5c57c9bf815dc3881c6dbcdd77987434524ad
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-06 23:44:22 +0300

    Add gift certificate DTO validation annotations

--- model/src/main/java/com/epam/esm/domain/dto/GiftCertificateDto.java
+++ model/src/main/java/com/epam/esm/domain/dto/GiftCertificateDto.java
@@ -1,16 +1,35 @@
 package com.epam.esm.domain.dto;
 
 import com.epam.esm.domain.Tag;
+import com.epam.esm.domain.validation.DtoTag;
+import com.epam.esm.domain.validation.PatchDto;
+import com.epam.esm.domain.validation.SaveDto;
+import com.epam.esm.domain.validation.ValidationErrorCode;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class GiftCertificateDto {
     private Long id;
+
+    @NotNull(groups = SaveDto.class, message = ValidationErrorCode.CERTIFICATE_NAME_NOT_NULL)
+    @Size(min = 1, max = 45, groups = {SaveDto.class, PatchDto.class}, message = ValidationErrorCode.INVALID_CERTIFICATE_NAME_LENGTH)
     private String name;
+
+    @NotNull(groups = SaveDto.class, message = ValidationErrorCode.CERTIFICATE_DESCRIPTION_NOT_NULL)
+    @Size(min = 1, max = 300, groups = {SaveDto.class, PatchDto.class}, message = ValidationErrorCode.INVALID_CERTIFICATE_DESCRIPTION_LENGTH)
     private String description;
+
+    @NotNull(groups = SaveDto.class, message = ValidationErrorCode.CERTIFICATE_PRICE_NOT_NULL)
+    @Positive(groups = {SaveDto.class, PatchDto.class}, message = ValidationErrorCode.INVALID_CERTIFICATE_PRICE)
     private Integer price;
+
+    @NotNull(groups = SaveDto.class, message = ValidationErrorCode.CERTIFICATE_DURATION_NOT_NULL)
+    @Positive(groups = {SaveDto.class, PatchDto.class}, message = ValidationErrorCode.INVALID_CERTIFICATE_DURATION)
     private Integer duration;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
---
+++ model/src/main/java/com/epam/esm/domain/validation/ValidationErrorCode.java
@@ -0,0 +1,17 @@
+package com.epam.esm.domain.validation;
+
+public class ValidationErrorCode {
+    public static final String CERTIFICATE_NAME_NOT_NULL = "40001";
+    public static final String INVALID_CERTIFICATE_NAME_LENGTH = "40002";
+    public static final String CERTIFICATE_DESCRIPTION_NOT_NULL = "40003";
+    public static final String INVALID_CERTIFICATE_DESCRIPTION_LENGTH = "40003";
+    public static final String CERTIFICATE_PRICE_NOT_NULL = "40004";
+    public static final String INVALID_CERTIFICATE_PRICE = "40005";
+    public static final String CERTIFICATE_DURATION_NOT_NULL = "40006";
+    public static final String INVALID_CERTIFICATE_DURATION = "40007";
+    public static final String DTO_TAG_LIST_NOT_NULL = "40008";
+    public static final String INVALID_DTO_TAG = "40009";
+
+    private ValidationErrorCode() {
+    }
+}
