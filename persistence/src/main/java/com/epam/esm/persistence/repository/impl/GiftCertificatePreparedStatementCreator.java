fcce2a5e7d79f28551b3bb281634d04b3c976635
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-06 09:30:46 +0300

    Refactor code

--- persistence/src/main/java/com/epam/esm/persistence/repository/impl/GiftCertificatePreparedStatementCreator.java
+++ persistence/src/main/java/com/epam/esm/persistence/repository/impl/GiftCertificatePreparedStatementCreator.java
@@ -14,15 +14,16 @@ public class GiftCertificatePreparedStatementCreator implements PreparedStatemen
     private static final String BASE_QUERY = "SELECT Gift_certificate.id, Gift_certificate.name, " +
             "Gift_certificate.description, Gift_certificate.price, Gift_certificate.duration, " +
             "Gift_certificate.create_date, Gift_certificate.last_update_date FROM Gift_certificate";
-    private static final String JOIN_TAGS = " JOIN Gift_certificate_has_Tag GchT on Gift_certificate.id = GchT.certificate JOIN Tag T on T.id = GchT.tag";
+    private static final String JOIN_TAGS = " JOIN Gift_certificate_has_Tag GchT on Gift_certificate.id = " +
+            "GchT.certificate JOIN Tag T on T.id = GchT.tag";
     private static final String WHERE = " WHERE ";
     private static final String TAG_NAME = "T.name LIKE  CONCAT('%', ?, '%')";
-    private static final String NAME_DESCRIPTION_PATTERN = "Gift_certificate.name LIKE CONCAT('%', ?, '%') OR Gift_certificate.description LIKE CONCAT('%', ?, '%')";
+    private static final String NAME_DESCRIPTION_PATTERN = "Gift_certificate.name LIKE CONCAT('%', ?, '%') OR " +
+            "Gift_certificate.description LIKE CONCAT('%', ?, '%')";
     private static final String AND = " AND ";
     private static final String ORDER_BY = " ORDER BY ";
 
     private final QueryFiltersConfig filtersConfig;
-    private boolean hasWhereSection;
     private boolean hasTagSection;
 
     public GiftCertificatePreparedStatementCreator(QueryFiltersConfig filtersConfig) {
@@ -54,10 +55,7 @@ public class GiftCertificatePreparedStatementCreator implements PreparedStatemen
     private void appendTagSection(StringBuilder queryBuilder) {
         if (filtersConfig.hasTag()) {
             queryBuilder.append(JOIN_TAGS);
-            if (!hasWhereSection) {
-                queryBuilder.append(WHERE);
-                hasWhereSection = true;
-            }
+            queryBuilder.append(WHERE);
             queryBuilder.append(TAG_NAME);
             hasTagSection = true;
         }
@@ -65,27 +63,39 @@ public class GiftCertificatePreparedStatementCreator implements PreparedStatemen
 
     private void appendSearchSection(StringBuilder queryBuilder) {
         if (filtersConfig.hasSearchPattern()) {
-            if (hasTagSection) {
-                queryBuilder.append(AND);
-            } else {
-                queryBuilder.append(WHERE);
-            }
+            resolveWhereOrAndKeyword(queryBuilder);
             queryBuilder.append(NAME_DESCRIPTION_PATTERN);
+        }
+    }
+
+    private void resolveWhereOrAndKeyword(StringBuilder queryBuilder) {
+        if (hasTagSection) {
+            queryBuilder.append(AND);
+        } else {
+            queryBuilder.append(WHERE);
         }
     }
 
     private void appendSortSection(StringBuilder queryBuilder) {
         if (filtersConfig.hasSortParameters()) {
-            Map<SortColumn, SortDirection> sortParameters = filtersConfig.getSortParameters();
             queryBuilder.append(ORDER_BY);
-            for (Map.Entry<SortColumn, SortDirection> parameter : sortParameters.entrySet()) {
-                queryBuilder.append(" ");
-                queryBuilder.append(parameter.getKey().getColumnName());
-                queryBuilder.append(" ");
-                queryBuilder.append(parameter.getValue().name());
-                queryBuilder.append(",");
-            }
-            queryBuilder.deleteCharAt(queryBuilder.lastIndexOf(","));
+            Map<SortColumn, SortDirection> sortParameters = filtersConfig.getSortParameters();
+            appendSortParameters(queryBuilder, sortParameters);
         }
+    }
+
+    private void appendSortParameters(StringBuilder queryBuilder, Map<SortColumn, SortDirection> sortParameters) {
+        for (Map.Entry<SortColumn, SortDirection> parameter : sortParameters.entrySet()) {
+            appendSortParameter(queryBuilder, parameter);
+        }
+        queryBuilder.deleteCharAt(queryBuilder.lastIndexOf(","));
+    }
+
+    private void appendSortParameter(StringBuilder queryBuilder, Map.Entry<SortColumn, SortDirection> parameter) {
+        queryBuilder.append(" ");
+        queryBuilder.append(parameter.getKey().getColumnName());
+        queryBuilder.append(" ");
+        queryBuilder.append(parameter.getValue().name());
+        queryBuilder.append(",");
     }
 }
