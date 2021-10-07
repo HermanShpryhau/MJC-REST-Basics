8c4014994c40c844f3603d4f64cf7c84f5aea05c
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-07 18:56:50 +0300

    Configure H2 db

--- persistence/src/main/resources/create_db.sql
+++ persistence/src/main/resources/create_db.sql
@@ -1,26 +1,28 @@
+DROP TABLE IF EXISTS Gift_certificate_has_Tag;
+DROP TABLE IF EXISTS Gift_certificate;
+DROP TABLE IF EXISTS Tag;
+
 create table Gift_certificate
 (
-    id               bigint unsigned auto_increment
-        primary key,
-    name             varchar(45)                         not null,
-    description      varchar(300)                        not null,
-    price            int unsigned                        not null,
-    duration         int                                 not null,
+    id               bigint auto_increment primary key,
+    name             varchar(45) not null,
+    description      varchar(300) not null,
+    price            int not null,
+    duration         int not null,
     create_date      timestamp default CURRENT_TIMESTAMP not null,
-    last_update_date timestamp default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP
+    last_update_date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 create table Tag
 (
-    id   bigint unsigned auto_increment
-        primary key,
+    id bigint auto_increment primary key,
     name varchar(45) not null
 );
 
 create table Gift_certificate_has_Tag
 (
-    certificate bigint unsigned not null,
-    tag         bigint unsigned not null,
+    certificate bigint not null,
+    tag bigint not null,
     primary key (certificate, tag),
     constraint fk_Gift_certificate_has_Tag_Gift_certificate
         foreign key (certificate) references Gift_certificate (id),
@@ -54,11 +56,3 @@ INSERT INTO Gift_certificate_has_Tag (certificate, tag) VALUES (4, 5);
 
 INSERT INTO Gift_certificate_has_Tag (certificate, tag) VALUES (5, 5);
 INSERT INTO Gift_certificate_has_Tag (certificate, tag) VALUES (5, 1);
-
-# create index fk_Gift_certificate_has_Tag_Gift_certificate_idx
-#     on Gift_certificate_has_Tag (certificate);
-#
-# create index fk_Gift_certificate_has_Tag_Tag1_idx
-#     on Gift_certificate_has_Tag (tag);
-
-
