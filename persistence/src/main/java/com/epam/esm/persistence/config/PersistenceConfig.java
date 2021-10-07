189968b97395fb3eaa1be74e9662fde9ee966cc8
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-07 18:56:34 +0300

    Add profiles

--- persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfig.java
+++ persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfig.java
@@ -5,34 +5,20 @@ import com.epam.esm.domain.Tag;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
 @Configuration
 @PropertySource("classpath:db.properties")
+@Import({ProdDataSourceConfig.class, DevDataSourceConfig.class})
 public class PersistenceConfig {
-    @Bean
-    public DataSource dataSource(@Value("${jdbc.username}") String username,
-                                 @Value("${jdbc.password}") String password,
-                                 @Value("${jdbc.driverClassName}") String className,
-                                 @Value("${jdbc.url}") String connectionUrl,
-                                 @Value("${jdbc.connections}") Integer connectionsNumber) {
-        BasicDataSource basicDataSource = new BasicDataSource();
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
-        basicDataSource.setDriverClassName(className);
-        basicDataSource.setUrl(connectionUrl);
-        basicDataSource.setMaxActive(connectionsNumber);
-        return basicDataSource;
-    }
-
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
