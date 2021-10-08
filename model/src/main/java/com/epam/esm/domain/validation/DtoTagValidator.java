3d3b9c8ddef6e1e47ede01c7f44a0501f4247d7f
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-08 16:15:36 +0300

    Fix validation logic

--- model/src/main/java/com/epam/esm/domain/validation/DtoTagValidator.java
+++ model/src/main/java/com/epam/esm/domain/validation/DtoTagValidator.java
@@ -9,7 +9,7 @@ public class DtoTagValidator implements ConstraintValidator<DtoTag, Tag> {
     @Override
     public boolean isValid(Tag tag, ConstraintValidatorContext context) {
         boolean valid = true;
-        if (tag.getName() == null || tag.getName().length() < 45 || tag.getName().length() > 1) {
+        if (tag.getName() == null || tag.getName().length() > 45 || tag.getName().length() < 1) {
             context.buildConstraintViolationWithTemplate("{tag.dto.constraint.message}");
             valid = false;
         }
