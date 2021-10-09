0deb7b01b2cea50ef31a5d2efc6fffb52414c9a8
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-09 09:21:50 +0300

    Add javadoc

--- service/src/main/java/com/epam/esm/service/TagService.java
+++ service/src/main/java/com/epam/esm/service/TagService.java
@@ -6,15 +6,44 @@ import com.epam.esm.domain.dto.GiftCertificateDto;
 import java.util.List;
 
 public interface TagService {
+
+    /**
+     * Saves tag to data source.
+     * @param tag Tag entity to save
+     * @return Saved tag entity
+     */
     Tag addTag(Tag tag);
 
+    /**
+     * Fetches all tags in data source.
+     * @return List of found tags
+     */
     List<Tag> fetchAllTags();
 
+    /**
+     * Fetches tag by ID.
+     * @param id ID of tag to fetch
+     * @return Found tag entity
+     */
     Tag fetchTagById(Long id);
 
+    /**
+     * Fetches tag by name.
+     * @param name Name of tag to fetch
+     * @return Found tag entity
+     */
     Tag fetchTagByName(String name);
 
+    /**
+     * Fetches tags associated with gift certificate.
+     * @param id ID of gift certificate to find tags for
+     * @return List of associated tags
+     */
     List<GiftCertificateDto> fetchAssociatedCertificates(Long id);
 
+    /**
+     * Deletes tag from data source
+     * @param id ID of tag entity to delete
+     */
     void deleteTag(Long id);
 }
