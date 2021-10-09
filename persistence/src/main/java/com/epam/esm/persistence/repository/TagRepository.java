b556a5d36994594f7bba096eb64ded6789fcfe7c
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-09 08:59:14 +0300

    Add javadoc

--- persistence/src/main/java/com/epam/esm/persistence/repository/TagRepository.java
+++ persistence/src/main/java/com/epam/esm/persistence/repository/TagRepository.java
@@ -7,7 +7,17 @@ import java.util.List;
 
 public interface TagRepository extends CrudRepository<Tag> {
 
+    /**
+     * Finds all gift certificates associated with tag
+     * @param tagId ID of tag
+     * @return List of associated gift certificates
+     */
     List<GiftCertificate> findAssociatedGiftCertificates(Long tagId);
 
+    /**
+     * Finds tag by name
+     * @param name Name of tag to search
+     * @return Found tag or {@code null}
+     */
     Tag findByName(String name);
 }
