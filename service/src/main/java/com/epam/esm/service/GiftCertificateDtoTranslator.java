9d1ad8e914522533d0f747bcae23d89f8516bacd
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-09 09:03:34 +0300

    Add javadoc

--- service/src/main/java/com/epam/esm/service/GiftCertificateDtoTranslator.java
+++ service/src/main/java/com/epam/esm/service/GiftCertificateDtoTranslator.java
@@ -7,9 +7,25 @@ import com.epam.esm.domain.dto.GiftCertificateDto;
 import java.util.List;
 
 public interface GiftCertificateDtoTranslator {
+    /**
+     * Translates gift certificate entity to gift certificate DTO.
+     * @param certificate Gift certificate entity
+     * @param tags List of associated tags
+     * @return Gift certificate DTO
+     */
     GiftCertificateDto giftCertificateToDto(GiftCertificate certificate, List<Tag> tags);
 
+    /**
+     * Translates gift certificate entity to gift certificate DTO. Associated tags are fetched from data source.
+     * @param certificate Gift certificate entity
+     * @return Gift certificate DTO
+     */
     GiftCertificateDto giftCertificateToDto(GiftCertificate certificate);
 
+    /**
+     * Transalte gift certificate DTO to gift certificate entity
+     * @param dto Gift certificate DTO
+     * @return Gift certificate entity object
+     */
     GiftCertificate dtoToGiftCertificate(GiftCertificateDto dto);
 }
