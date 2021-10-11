cc4970c5ed1b291bc526f865ca03da2682387e8d
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-12 01:00:38 +0300

    Add test PersistenceConfig

---
+++ persistence/src/test/java/com/epam/esm/persistence/config/TestPersistenceConfig.java
@@ -0,0 +1,63 @@
+package com.epam.esm.persistence.config;
+
+import com.epam.esm.domain.GiftCertificate;
+import com.epam.esm.domain.Tag;
+import com.epam.esm.persistence.repository.GiftCertificateRepository;
+import com.epam.esm.persistence.repository.impl.GiftCertificateRepositoryImpl;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.*;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.epam.esm")
+public class TestPersistenceConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("create_db.sql")
+                .build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public RowMapper<GiftCertificate> giftCertificateRowMapper() {
+        return new BeanPropertyRowMapper<>(GiftCertificate.class);
+    }
+
+    @Bean
+    public RowMapper<Tag> tagRowMapper() {
+        return new BeanPropertyRowMapper<>(Tag.class);
+    }
+
+    @Bean
+    @Qualifier("GiftCertificateJdbcInsert")
+    public SimpleJdbcInsert giftCertificateJdbcInsert(JdbcTemplate jdbcTemplate) {
+        return new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("Gift_certificate")
+                .usingGeneratedKeyColumns("id")
+                .usingColumns("name", "description", "price", "duration");
+    }
+
+    @Bean
+    @Qualifier("TagJdbcInsert")
+    public SimpleJdbcInsert tagJdbcInsert(JdbcTemplate jdbcTemplate) {
+        return new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("Tag")
+                .usingGeneratedKeyColumns("id");
+    }
+}
