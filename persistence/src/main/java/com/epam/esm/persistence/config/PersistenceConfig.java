50be466135784dee4c82391f2190098351b26613
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-12 00:59:41 +0300

    Optimize imports

--- persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfig.java
+++ persistence/src/main/java/com/epam/esm/persistence/config/PersistenceConfig.java
@@ -2,17 +2,13 @@ package com.epam.esm.persistence.config;
 
 import com.epam.esm.domain.GiftCertificate;
 import com.epam.esm.domain.Tag;
-import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -20,7 +16,6 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:db.properties")
 @Import({ProdDataSourceConfig.class, DevDataSourceConfig.class})
 public class PersistenceConfig {
     @Bean
