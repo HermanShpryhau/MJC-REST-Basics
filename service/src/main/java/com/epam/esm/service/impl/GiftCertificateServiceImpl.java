5d6868d063a39a03f73fddf7e73f8a86292bd612
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-16 22:18:44 +0300

    Remove import wildcard

--- service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
+++ service/src/main/java/com/epam/esm/service/impl/GiftCertificateServiceImpl.java
@@ -16,7 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
