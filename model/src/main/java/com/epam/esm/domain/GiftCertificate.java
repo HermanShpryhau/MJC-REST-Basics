960f3ef267e623e81ba2854a42643312f03d6565
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-17 15:47:51 +0300

    Add persistence annotations

--- model/src/main/java/com/epam/esm/domain/GiftCertificate.java
+++ model/src/main/java/com/epam/esm/domain/GiftCertificate.java
@@ -1,9 +1,15 @@
 package com.epam.esm.domain;
 
-import java.io.Serializable;
+import javax.persistence.*;
 import java.sql.Timestamp;
 
-public class GiftCertificate extends AbstractEntity implements Serializable {
+@Entity
+@Table(name = "Gift_certificate")
+public class GiftCertificate {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
     private String name;
     private String description;
     private int price;
@@ -16,13 +22,21 @@ public class GiftCertificate extends AbstractEntity implements Serializable {
 
     public GiftCertificate(Long id, String name, String description, int price, int duration, Timestamp createDate,
                            Timestamp lastUpdateDate) {
-        super(id);
+        this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.duration = duration;
         this.createDate = createDate;
         this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
