0e088bc60164030caeff0067ad24262ce11d028b
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-18 13:28:08 +0300

    Change generation type

--- model/src/main/java/com/epam/esm/domain/AbstractEntity.java
+++ model/src/main/java/com/epam/esm/domain/AbstractEntity.java
@@ -8,7 +8,7 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public abstract class AbstractEntity {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     protected AbstractEntity() {
