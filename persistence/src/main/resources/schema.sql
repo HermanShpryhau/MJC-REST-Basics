cdd835d9f8a3156c47daede4278d0ca8b762353f
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-18 13:27:46 +0300

    Add new tables to schema

--- persistence/src/main/resources/schema.sql
+++ persistence/src/main/resources/schema.sql
@@ -30,3 +30,29 @@ create table Gift_certificate_has_Tag
         foreign key (tag) references Tag (id) on delete cascade
 );
 
+create table User
+(
+    id   bigint auto_increment primary key,
+    name varchar(50) not null,
+    constraint User_id_uindex
+        unique (id)
+);
+
+create table Orders
+(
+    id              bigint auto_increment primary key,
+    user_id         bigint                              not null,
+    certificate_id  bigint                              not null,
+    quantity        int                                 not null,
+    total_price     int                                 not null,
+    submission_date timestamp default CURRENT_TIMESTAMP not null,
+    constraint Order_id_uindex
+        unique (id),
+    constraint Order_Gift_certificate_id_fk
+        foreign key (certificate_id) references Gift_certificate (id)
+            on delete restrict,
+    constraint Order_User_id_fk
+        foreign key (user_id) references User (id)
+            on delete cascade
+);
+
