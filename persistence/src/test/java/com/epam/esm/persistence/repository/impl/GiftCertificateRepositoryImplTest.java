d958b31a58511d18d75e6b01e9a7fd70eda0c676
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-20 11:18:08 +0300

    Optimize imports

--- persistence/src/test/java/com/epam/esm/persistence/repository/impl/GiftCertificateRepositoryImplTest.java
+++ persistence/src/test/java/com/epam/esm/persistence/repository/impl/GiftCertificateRepositoryImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
