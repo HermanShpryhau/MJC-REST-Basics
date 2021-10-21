316f9f7c0612d9b34145fa06ad9180cd9d317cbd
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-21 20:54:43 +0300

    Restrict generics

--- model/src/main/java/com/epam/esm/domain/dto/serialization/DtoSerializer.java
+++ model/src/main/java/com/epam/esm/domain/dto/serialization/DtoSerializer.java
@@ -1,11 +1,13 @@
 package com.epam.esm.domain.dto.serialization;
 
+import com.epam.esm.domain.AbstractEntity;
+
 /**
  * Class serializes DTO to persisted entities and vice versa.
  * @param <D> Type of DTO
  * @param <E> Type of entity
  */
-public interface DtoSerializer<D, E> {
+public interface DtoSerializer<D, E extends AbstractEntity> {
     D dtoFromEntity(E entity);
 
     E dtoToEntity(D dto);
