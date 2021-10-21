8e692a86e08961ae22c06aacda89b651398d7f59
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-21 19:05:46 +0300

    Update javadoc

--- persistence/src/main/java/com/epam/esm/persistence/repository/GiftCertificateRepository.java
+++ persistence/src/main/java/com/epam/esm/persistence/repository/GiftCertificateRepository.java
@@ -9,15 +9,17 @@ import java.util.List;
 public interface GiftCertificateRepository extends CrudRepository<GiftCertificate> {
 
     /**
-     * Finds gift certificates according to filtering parameters supplied with {@link QueryFiltersConfig}
+     * Finds gift certificates according to filtering parameters supplied with {@link QueryFiltersConfig}.
      *
      * @param config Filter configuration
+     * @param page Index of page
+     * @param size Size of page
      * @return List of found gift certificates
      */
-    List<GiftCertificate> findWithFilters(QueryFiltersConfig config, int page, int count);
+    List<GiftCertificate> findWithFilters(QueryFiltersConfig config, int page, int size);
 
     /**
-     * Finds tags associated with gift certificate
+     * Finds tags associated with gift certificate.
      *
      * @param certificateId ID of gift certificate
      * @return List of associated tags
