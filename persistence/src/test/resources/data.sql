2f39bcade6f241cb9fc94c9134f8de70e2cb7660
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-21 22:24:50 +0300

    Add test data for new tables

--- persistence/src/test/resources/data.sql
+++ persistence/src/test/resources/data.sql
@@ -19,3 +19,17 @@ INSERT INTO Gift_certificate_has_Tag (certificate, tag)
 VALUES (2, 2);
 INSERT INTO Gift_certificate_has_Tag (certificate, tag)
 VALUES (2, 3);
+
+INSERT INTO User (id, name)
+VALUES (1, 'User 1');
+INSERT INTO User (id, name)
+VALUES (2, 'User 2');
+
+INSERT INTO Orders (id, user_id, certificate_id, quantity, total_price, submission_date)
+VALUES (1, 1, 1, 2, 238, '2021-09-27 18:13:56');
+INSERT INTO Orders (id, user_id, certificate_id, quantity, total_price, submission_date)
+VALUES (2, 1, 2, 3, 573, '2021-09-27 18:13:56');
+INSERT INTO Orders (id, user_id, certificate_id, quantity, total_price, submission_date)
+VALUES (3, 2, 1, 4, 476, '2021-09-27 18:13:56');
+INSERT INTO Orders (id, user_id, certificate_id, quantity, total_price, submission_date)
+VALUES (4, 2, 2, 5, 955, '2021-09-27 18:13:56');
