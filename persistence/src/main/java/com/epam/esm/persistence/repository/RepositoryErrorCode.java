98347af283c254c778ec54628acd0c45c65ecbc2
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-23 20:06:52 +0300

    Refactor repository exception

---
+++ persistence/src/main/java/com/epam/esm/persistence/repository/RepositoryErrorCode.java
@@ -0,0 +1,7 @@
+package com.epam.esm.persistence.repository;
+
+public final class RepositoryErrorCode {
+    public static final String OPERATION_NOT_SUPPORTED = "40301";
+
+    private RepositoryErrorCode() {}
+}
