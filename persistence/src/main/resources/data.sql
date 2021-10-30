44f0886b9d3cdff2a00d2d6901f9e5d7e89cc635
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-10-30 18:28:27 +0300

    Update test environment setup

--- persistence/src/main/resources/data.sql
+++ persistence/src/main/resources/data.sql
@@ -1,3 +1,73 @@
+drop table if exists Orders;
+drop table if exists Gift_certificate_has_Tag;
+drop table if exists User;
+drop table if exists Tag;
+drop table if exists Gift_certificate;
+
+create table if not exists Gift_certificate
+(
+    id                  bigint auto_increment primary key,
+    name                varchar(45)  not null,
+    description         varchar(300) not null,
+    price               int          not null,
+    duration            int          not null,
+    create_date         timestamp             default CURRENT_TIMESTAMP not null,
+    last_update_date    timestamp    not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    operation           varchar(10),
+    operation_timestamp timestamp
+);
+
+create table if not exists Tag
+(
+    id                  bigint auto_increment primary key,
+    name                varchar(45) not null unique,
+    operation           varchar(10),
+    operation_timestamp timestamp
+);
+
+create table if not exists Gift_certificate_has_Tag
+(
+    certificate bigint not null,
+    tag         bigint not null,
+    primary key (certificate, tag),
+    constraint fk_Gift_certificate_has_Tag_Gift_certificate
+        foreign key (certificate) references Gift_certificate (id) on delete cascade,
+    constraint fk_Gift_certificate_has_Tag_Tag1
+        foreign key (tag) references Tag (id) on delete restrict
+);
+
+create table if not exists User
+(
+    id                  bigint auto_increment primary key,
+    name                varchar(50) not null,
+    operation           varchar(10),
+    operation_timestamp timestamp,
+    constraint User_id_uindex
+        unique (id)
+);
+
+create table if not exists Orders
+(
+    id                  bigint auto_increment primary key,
+    user_id             bigint                              not null,
+    certificate_id      bigint                              not null,
+    quantity            int                                 not null,
+    total_price         int                                 not null,
+    submission_date     timestamp default CURRENT_TIMESTAMP not null,
+    operation           varchar(10),
+    operation_timestamp timestamp,
+    constraint Order_id_uindex
+        unique (id),
+    constraint Order_Gift_certificate_id_fk
+        foreign key (certificate_id) references Gift_certificate (id)
+            on delete restrict,
+    constraint Order_User_id_fk
+        foreign key (user_id) references User (id)
+            on delete restrict
+);
+
+
+
 INSERT INTO Gift_certificate (id, name, description, price, duration, create_date, last_update_date)
 VALUES (1, 'Certificate 1', 'Description 1', 119, 25, '2021-09-28 18:13:56', '2021-09-28 18:13:56');
 INSERT INTO Gift_certificate (id, name, description, price, duration, create_date, last_update_date)
--- persistence/src/test/java/com/epam/esm/persistence/repository/impl/GiftCertificateRepositoryImplTest.java
+++ persistence/src/test/java/com/epam/esm/persistence/repository/impl/GiftCertificateRepositoryImplTest.java
@@ -6,18 +6,20 @@ import com.epam.esm.persistence.repository.GiftCertificateRepository;
 import com.epam.esm.persistence.repository.filter.GiftCertificatesFilterConfig;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
+@Transactional
+@ActiveProfiles("dev")
+@ComponentScan(basePackages = "com.epam.esm")
 class GiftCertificateRepositoryImplTest {
     private static final List<Tag> IN_DB_TAGS = Arrays.asList(
             new Tag(1L, "Tag 1"),
@@ -28,16 +30,11 @@ class GiftCertificateRepositoryImplTest {
     private static final List<Tag> CERTIFICATE_2_TAGS = IN_DB_TAGS.subList(1, 3);
 
     private static final List<GiftCertificate> IN_DB_CERTIFICATES = Arrays.asList(
-            new GiftCertificate(1L, "Certificate 1", "Description 1", 119, 25,
-                    LocalDateTime.parse("2021-09-28T18:13:56"),
-                    LocalDateTime.parse("2021-09-28T18:13:56")),
-            new GiftCertificate(2L, "Certificate 2", "Description 2", 191, 28,
-                    LocalDateTime.parse("2021-09-27T18:13:56"),
-                    LocalDateTime.parse("2021-09-27T18:13:56"))
+            new GiftCertificate(),
+            new GiftCertificate()
     );
 
-    private static final GiftCertificate TEST_CERTIFICATE = new GiftCertificate(0L, "test", "test", 12, 12,
-            null, null);
+    private static final GiftCertificate TEST_CERTIFICATE = new GiftCertificate();
     private final GiftCertificateRepository repository;
 
     @Autowired
@@ -85,18 +82,6 @@ class GiftCertificateRepositoryImplTest {
                 GiftCertificatesFilterConfig.builder().withTags(Collections.singletonList("Tag 123")).build();
         List<GiftCertificate> certificates = repository.findWithFilters(config, 1, 10);
         Assertions.assertEquals(Collections.emptyList(), certificates);
-    }
-
-    @Test
-    void findAssociatedTags() {
-        List<Tag> tags = repository.findAssociatedTags(2L);
-        Assertions.assertEquals(CERTIFICATE_2_TAGS, tags);
-    }
-
-    @Test
-    void findAssociatedTagsForNonExistingCertificate() {
-        List<Tag> tags = repository.findAssociatedTags(123L);
-        Assertions.assertEquals(Collections.emptyList(), tags);
     }
 
     @Test
