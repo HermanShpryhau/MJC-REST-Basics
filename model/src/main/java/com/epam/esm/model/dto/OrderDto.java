db2f00ca32332e34b100ce5ea7d90707e8ea3d86
Herman Shpryhau <herman.shpryhau@gmail.com>
2021-11-09 15:47:03 +0300

    Use lombok for DTOs

--- model/src/main/java/com/epam/esm/model/dto/OrderDto.java
+++ model/src/main/java/com/epam/esm/model/dto/OrderDto.java
@@ -2,11 +2,17 @@ package com.epam.esm.model.dto;
 
 import com.epam.esm.model.validation.ValidationErrorCode;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OrderDto implements DataTransferObject {
     private Long id;
 
@@ -24,103 +30,4 @@ public class OrderDto implements DataTransferObject {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS")
     private LocalDateTime submissionDate;
-
-    public OrderDto() {
-    }
-
-    public OrderDto(Long id, UserDto user, GiftCertificateDto giftCertificate, Integer quantity, Integer totalPrice,
-                    LocalDateTime submissionDate) {
-        this.id = id;
-        this.user = user;
-        this.giftCertificate = giftCertificate;
-        this.quantity = quantity;
-        this.totalPrice = totalPrice;
-        this.submissionDate = submissionDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public UserDto getUser() {
-        return user;
-    }
-
-    public void setUser(UserDto user) {
-        this.user = user;
-    }
-
-    public GiftCertificateDto getGiftCertificate() {
-        return giftCertificate;
-    }
-
-    public void setGiftCertificate(GiftCertificateDto giftCertificate) {
-        this.giftCertificate = giftCertificate;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Integer getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(Integer totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public LocalDateTime getSubmissionDate() {
-        return submissionDate;
-    }
-
-    public void setSubmissionDate(LocalDateTime submissionDate) {
-        this.submissionDate = submissionDate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || o.getClass() != getClass()) return false;
-
-        OrderDto orderDto = (OrderDto) o;
-
-        if (!id.equals(orderDto.id)) return false;
-        if (!user.equals(orderDto.user)) return false;
-        if (!giftCertificate.equals(orderDto.giftCertificate)) return false;
-        if (!quantity.equals(orderDto.quantity)) return false;
-        if (!totalPrice.equals(orderDto.totalPrice)) return false;
-        return submissionDate.equals(orderDto.submissionDate);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id.hashCode();
-        result = 31 * result + user.hashCode();
-        result = 31 * result + giftCertificate.hashCode();
-        result = 31 * result + quantity.hashCode();
-        result = 31 * result + totalPrice.hashCode();
-        result = 31 * result + submissionDate.hashCode();
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "OrderDto{" +
-                "id=" + id +
-                ", user=" + user +
-                ", giftCertificate=" + giftCertificate +
-                ", quantity=" + quantity +
-                ", totalPrice=" + totalPrice +
-                ", submissionDate=" + submissionDate +
-                '}';
-    }
 }
